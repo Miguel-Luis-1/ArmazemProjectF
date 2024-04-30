79ee33f71187e2b61386de92371728da8596641b
Miguel Luis <migueluis1701@gmail.com>
2024-04-30 09:58:26 -0300

    Init Commit

---
+++ android/app/src/main/kotlin/com/example/armazemf/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.armazemf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:armazemf/screens/home/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomePage());
+}
+
