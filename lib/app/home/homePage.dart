db984b94a2541e81d26ec35a124bd9496dd6d598
Miguel Luis <migueluis1701@gmail.com>
2024-05-01 12:07:57 -0300

    Drawer e cores
    * Adição do drawer;
    * Adição das cores base;

--- lib/app/home/homePage.dart
+++ lib/app/home/homePage.dart
@@ -11,14 +11,40 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
+      backgroundColor: const Color(0xFFE8F1F2),
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: const Color(0xFF247BA0),
         title: const Text('Lista de produtos', 
         style: TextStyle(
           color: Colors.white,
           fontWeight: FontWeight.bold,        
         ),
         textDirection: TextDirection.ltr
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              child: Text('Cabeçalho do Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
       body: const Column(
