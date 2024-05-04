8d3ed3c59f60d1701e4149cd2f417e6163f52d9c
Miguel Luis <migueluis1701@gmail.com>
2024-05-04 14:33:36 -0300

    Base do conteúdo da pagina de cadastro
    * Fata ajustes e perfumaria

--- lib/app/pages/cadastro_page.dart
+++ lib/app/pages/cadastro_page.dart
@@ -1,8 +1,11 @@
+import 'package:armazemf/app/stores/cadastro_page_store.dart';
 import 'package:armazemf/app/widgets/back_dialog.dart';
 import 'package:armazemf/app/widgets/base_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class CadastroPage extends StatefulWidget {
   const CadastroPage({super.key});
@@ -12,6 +15,16 @@ class CadastroPage extends StatefulWidget {
 }
 
 class _CadastroPageState extends State<CadastroPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  CadastroPageStore store = Modular.get<CadastroPageStore>();
+
+  @override
+  void initState() {
+    store.dispose();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Observer(builder: (context) {
@@ -21,9 +34,79 @@ class _CadastroPageState extends State<CadastroPage> {
         child: BasePage(
           drawer: false,
           title: 'Fazer Cadastro',
-          body: Container(),
+          body: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Center(
+                    child: Text(
+                      'Faça o seu cadastro',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: store.nomeController,
+                    decoration: const InputDecoration(labelText: 'Nome'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Por favor, insira seu nome';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: store.emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Por favor, insira seu email';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: store.senhaController,
+                    decoration: const InputDecoration(labelText: 'Senha'),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Por favor, insira sua senha';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: store.codigoEmpresaController,
+                    decoration: const InputDecoration(labelText: 'Código da Empresa'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    validator: (value) {
+                       if (value!.isEmpty) {
+                        return 'Por favor, insira o codigo';
+                      }
+                    }, // Only numbers can be entered
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                      }
+                    },
+                    child: const Text('Cadastrar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       );
     });
   }
 }
+
--- lib/app/stores/cadastro_page_store.dart
+++ lib/app/stores/cadastro_page_store.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
 part 'cadastro_page_store.g.dart';
@@ -5,7 +6,21 @@ part 'cadastro_page_store.g.dart';
 class CadastroPageStore = CadastroPageStoreBase with _$CadastroPageStore;
 
 abstract class CadastroPageStoreBase with Store {
+  @observable
+  TextEditingController nomeController = TextEditingController();
+  @observable
+  TextEditingController emailController = TextEditingController();
+  @observable
+  TextEditingController senhaController = TextEditingController();
+  @observable
+  TextEditingController codigoEmpresaController = TextEditingController();
 
- 
- 
+
+  @action
+  dispose(){
+    nomeController.clear();
+    emailController.clear();
+    senhaController.clear();
+    codigoEmpresaController.clear();
+  }
 }
